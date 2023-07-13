2cbba95026f5b496482b33294a332633fe3f1bc9
harmlessman <harmlessman17@gmail.com>
2023-07-14 00:08:11 +0900

    Add ScreenUtilInit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'first_run_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'pages/home_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'pages/detail_search_page.dart';
 import 'pages/search_page.dart';
 import 'pages/movie_rating_info.dart';
 import 'pages/update_page.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  if (prefs.getBool('is_first_run') == null) {
-    prefs.setBool('is_first_run', true);
-    runApp(const MaterialApp(
-      home: FirstRunScreen(),
-    ));
-  } else {
-    runApp(MyApp());
-  }
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding); //인트로 시작
+
+  //SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(const MyApp());
+  // if (prefs.getBool('is_first_run') == null) {
+  //   prefs.setBool('is_first_run', true);
+  //   runApp(MyApp());
+  // } else {
+  //   runApp(MyApp());
+  // }
+  sleep(const Duration(seconds: 1)); //인트로 최소 유지시간 보장
+  FlutterNativeSplash.remove(); //인트로 종료
 }
 
 class MyApp extends StatelessWidget {
@@ -27,19 +36,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: '아이의 영화',
-        debugShowCheckedModeBanner: false,
-        initialRoute:'/',
-        routes: {
-          '/': (context) => HomePage(),
-          '/search': (context) => SearchPage(),
-          '/detail_search': (context) => DetailSearchPage(),
-          '/rating_info': (context) => MovieRatingInfoPage(),
-          '/update': (context) => UpdatePage(),
-        }
-        );
+    return ScreenUtilInit(
+        designSize: const Size(540.0, 1152.0),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+              title: '아이의 영화',
+              debugShowCheckedModeBanner: false,
+              initialRoute: '/',
+              routes: {
+                '/': (context) => HomePage(),
+                '/search': (context) => SearchPage(),
+                '/detail_search': (context) => DetailSearchPage(),
+                '/rating_info': (context) => MovieRatingInfoPage(),
+                '/update': (context) => UpdatePage(),
+              });
+        });
   }
 }
-
-

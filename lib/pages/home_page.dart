880abc3147db62f4c11781b7f793ef839a11c549
harmlessman <harmlessman17@gmail.com>
2023-08-25 10:18:43 +0900

    Change background

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:child_movie/setting.dart';
+import 'package:child_movie/setting/setting_pages/setting_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
@@ -96,105 +96,126 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffFEEBB6),
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          //iconTheme: const IconThemeData(color: Colors.red),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: Colors.grey,
-              ),
-              iconSize: 40,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Setting(),
-                  ),
-                );
-              },
-            )
-          ]),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20.0.h,
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.green.shade100,
+            Colors.yellow.shade100,
+            Colors.orange.shade100,
+            Colors.red.shade100,
+            //Color(0xffFEEBB6),
+            //Colors.white
+          ], stops: [
+            0.25,
+            0.5,
+            0.75,
+            1
+          ],
+
+
           ),
-          Text(
-            "안녕하세요 사용자님!",
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              color: Color(0xff2f5d62).withOpacity(0.77),
-              fontSize: 30.0.sp,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 10.0.h,
-          ),
-          Text(
-            "사용하실 기능을 선택해주세요",
-            style: TextStyle(
-              //fontFamily: 'normal',
-              decoration: TextDecoration.none,
-              color: Color(0xff2f5d62),
-              fontSize: 40.0.sp,
-              fontWeight: FontWeight.bold,
-              height: 1.2.h,
-            ),
-          ),
-          SizedBox(
-            height: 100.h,
-          ),
-          Expanded(
-            child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40.0),
-                    topRight: Radius.circular(40.0),
-                  ),
+
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            //iconTheme: const IconThemeData(color: Colors.red),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.grey,
                 ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 540.h,
-                      child: GridView(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 24.0.w, vertical: 24.0.h),
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 1,
-                            crossAxisSpacing: 24.0.h,
-                            mainAxisSpacing: 24.0.h,
-                          ),
-                          children: <Widget>[
-                            optionBox(options[0], Colors.green.shade300),
-                            optionBox(options[1], Colors.yellow.shade300),
-                            optionBox(options[2], Colors.orange.shade300),
-                            optionBox(options[3], Colors.red.shade300),
-                          ]),
+                iconSize: 40,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SettingPage(),
                     ),
-                    Flexible(
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          'assets/age_rating_image/sum.png',
-                          width: 150.w,
-                          height: 150.h,
+                  );
+                },
+              )
+            ]),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20.0.h,
+            ),
+            Text(
+              "안녕하세요 사용자님!",
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                color: Color(0xff2f5d62).withOpacity(0.77),
+                fontSize: 30.0.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0.h,
+            ),
+            Text(
+              "사용하실 기능을 선택해주세요",
+              style: TextStyle(
+                //fontFamily: 'normal',
+                decoration: TextDecoration.none,
+                color: Color(0xff2f5d62),
+                fontSize: 40.0.sp,
+                fontWeight: FontWeight.bold,
+                height: 1.2.h,
+              ),
+            ),
+            SizedBox(
+              height: 100.h,
+            ),
+            Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0),
+                      topRight: Radius.circular(40.0),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 540.h,
+                        child: GridView(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24.0.w, vertical: 24.0.h),
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 1,
+                              crossAxisSpacing: 24.0.h,
+                              mainAxisSpacing: 24.0.h,
+                            ),
+                            children: <Widget>[
+                              optionBox(options[0], Colors.green.shade300),
+                              optionBox(options[1], Colors.yellow.shade300),
+                              optionBox(options[2], Colors.orange.shade300),
+                              optionBox(options[3], Colors.red.shade300),
+                            ]),
+                      ),
+                      Flexible(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/age_rating_image/sum.png',
+                            width: 150.w,
+                            height: 150.h,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )),
-          ),
-        ],
+                    ],
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
